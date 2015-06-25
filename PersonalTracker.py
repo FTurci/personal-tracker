86f442769f14fb8376304bff7e5c01580be458ad
Francesco Turci <f.turci@bristol.ac.uk>
2015-06-25 17:08:08 +0100

    Added input parser

--- PersonalTracker.py
+++ PersonalTracker.py
@@ -4,6 +4,10 @@
 import pylab as pl
 import sys
 import time
+import argparse
+# install with :
+# pip install colorama
+from colorama import Fore as F
 
 def view_slice(Array,Slice, axis=2):
     # view a slice 
@@ -17,20 +21,32 @@ def view_slice(Array,Slice, axis=2):
     pl.colorbar()
 
 
-if(len(sys.argv)<3):
-    print("Usage: python PersonalTracker.py filename cutting_distance")
-    sys.exit(0)
+parser=argparse.ArgumentParser(description="A particle tracking code in Python based on morphological transformations."
+    )
+parser.add_argument("filename", type=str, help="The .hex series filename. It has to be formed as string1_string2_string3_integer_sizex_sizey_sizez_*.hex")
+parser.add_argument("--cutoff",  type=float, help="shortest contact distance")
+parser.add_argument("--opening",  type=bool, help="Turn the Opening operation on/off (bool)")
+parser.add_argument("--erosion",  type=bool, help="Turn the Erosion operation on/off (bool)")
+parser.add_argument("--exponent",  type=float, help="exponent E for the intensity transform I^E")
+parser.set_defaults(cutoff=13,exponent=1., opening=False, erosion=False)
+args=parser.parse_args()
 
+filename=args.filename
+cutoff=args.cutoff
+exponent=args.exponent
+opening=args.opening
+erosion=args.erosion
 
-filename=sys.argv[1]
-diameter=float(sys.argv[2])
+print ("\nHello! I am going to track "+F.CYAN+"%s "+F.RESET+"\n")%filename
+print ("Cutoff "+F.CYAN+"%g"+F.RESET+" exponent "+F.CYAN+"%g"+F.RESET)%(cutoff,exponent)
 
-print "\nHello! I am going to track %s \n\n- Reading data..."%filename
+print "\n- Reading data..."
 start=time.time()
 
 # read it 8 bits by 8 bits
 data=pl.fromfile(filename, dtype=pl.int8)
 # find the image sizes from the filename
+# print filename.split('_')
 Sizes=filename.split('_')[4:7]
 # needs filenames like Series028_decon_conf_1_256_256_256_0.019917_0.019917_0.020424.hex
 
@@ -48,7 +64,7 @@ Cross=[[
     [1,1,1],
     [0,1,0],],
    [[0,1,0],
-    [1,1,1],
+    [1,2,1],
     [0,1,0],],
    [[0,1,0],
     [1,1,1],
@@ -59,34 +75,37 @@ from scipy.ndimage.filters import maximum_filter, gaussian_filter,rank_filter, m
 from scipy.ndimage.morphology import grey_opening,grey_closing, grey_erosion
 from scipy.ndimage.measurements import label,center_of_mass,maximum_position
 
-
 # ===================== MANIPULATING THE IMAGE ==================
 # remornalising the input. 
-# If needed, a nonlinear transformation can be performed changing the Exponent variable
-# Larger exponennts suppress the low intensity region of the spectrum 
+# If needed, a nonlinear transformation can be performed changing the exponent variable
+# Larger exponents suppress the low intensity region of the spectrum 
+print "====> Starting the manipulations...\n"
 
-Exponent=1.
-Data=((Data/float(Data.max()*1) )**Exponent*255).astype(int)
+if (exponent!= 1):
+    print "- Nonlinear Stretching..."
+    Data=((Data/float(Data.max()) )**exponent*255).astype(int)
 
-print "- Morphological Opening..."
-Step1=grey_opening(Data, size=(3,3,3))
-pl.save("Step1", Step1)
+if(opening):
+    print "- Morphological Opening..."
+    Data=grey_opening(Data, structure=Cross)
+# pl.save("Step1", Step1)
 
-print "- Morphological Erosion..."
-Step2=grey_erosion(Step1, structure=Cross)
-pl.save("Step2", Step2)
+if(erosion):
+    print "- Morphological Erosion..."
+    Data=grey_erosion(Data, structure=Cross)
+# pl.save("Step2", Step2)
 
 # Remark: one could keep on with other transformations, other kernels and so on
 # To do so, I would reccomend to use ipython, and eventually load the partial results 
 
-FinalStep=Step2
+FinalStep=Data
 # Uncomment to view a slice
 # view_slice(Step,200)
 # pl.savefig("test.png")
 print "- Finding maxima..."
-local_max=maximum_filter(FinalStep, size=(diameter/2, diameter/2, diameter/2))==FinalStep
+local_max=maximum_filter(FinalStep, size=(cutoff/2, cutoff/2, cutoff/2))==FinalStep
 Labelled, Num=label(local_max)
-print "\n====> Found", Num,"maxima\n"
+print "\n====> Found"+F.RED, Num,F.RESET+"maxima\n"
 
 # if too many centres are found, the memory will explode at the next operation
 if(Num>15000):
@@ -107,8 +126,8 @@ dists=squareform(pdist(Positions))
 # exclude the case of self-distance
 pl.fill_diagonal(dists, pl.inf)
 
-print "- Cutting out particles with at least two overlaps at distance <", diameter,"..."
-test= (dists<diameter)
+print "- Cutting out particles with at least two overlaps at distance <", cutoff,"..."
+test= (dists<cutoff)
 to_be_deleted=[]
 for p in range(N):
     # removing only double overlaps
@@ -124,8 +143,8 @@ dists=squareform(pdist(No_overlaps_r1))
 # exclude the case of self-distance
 pl.fill_diagonal(dists, pl.inf)
 
-print "- Cutting out particles with overlaps at distance <", diameter,"..."
-test= (dists<diameter)
+print "- Cutting out particles with overlaps at distance <", cutoff,"..."
+test= (dists<cutoff)
 to_be_deleted=[]
 for p in range(No_overlaps_r1.shape[0]):
     if(pl.any(test[p][p:])):
@@ -134,7 +153,7 @@ overlaps=pl.array(to_be_deleted)
 No_overlaps_r2=pl.delete(No_overlaps_r1,overlaps, axis=0)
 
 
-print "\n====> Detected",No_overlaps_r2.shape[0],"particles.\n"
+print "\n====> Detected"+F.GREEN,No_overlaps_r2.shape[0],F.RESET+"particles.\n"
 
 # ======================== SAVING THE RESULT ===================== 
 # reorder the columns
